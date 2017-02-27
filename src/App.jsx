0caade8575a4bb7328f64775289f9a3e79cf40e7
Kevin Mann <kevinrmann@gmail.com>
2017-02-26 18:59:21 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header.jsx';
+
+import style from './sass/main.scss';
+
+class App extends React.Component {
+  render () {
+    return (
+      <Header />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render () {
+    return (
+      <header className="header">
+        <h1>React Header</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
