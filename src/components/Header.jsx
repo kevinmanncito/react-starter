667e1fff92b7f67198bb936b0cd09c13d749185b
Kevin Mann <kevinrmann@gmail.com>
2017-05-01 17:06:43 -0600

    Tweaking the web pack dev server settings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+
 class Header extends React.Component {
   render () {
     return (
       <header className="header">
-        <h1>React Header</h1>
+        <h1>React</h1>
+        <img src={require('../images/me.png')} />
       </header>
     );
   }
