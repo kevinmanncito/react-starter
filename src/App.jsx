55e3d0c176f1d3a450dd55ee6ca815cd73916966
Kevin Mann <kevinrmann@gmail.com>
2017-12-03 14:10:02 -0700

    Adding eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Header from './components/Header'
 
-
 class App extends React.Component {
   render () {
     return (
       <Header />
-    );
+    )
   }
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-
+import React from 'react'
 
 class Header extends React.Component {
   render () {
     return (
-      <header className="header">
+      <header className='header'>
         <h1>React</h1>
         <img src={require('../images/me.png')} />
       </header>
-    );
+    )
   }
 }
 
-export default Header;
+export default Header
